9159990a75012e0e868fecae5780c34f368deb22
thomas <130037065+thomas9291@users.noreply.github.com>
2023-07-10 14:37:49 +0200

    begin auth

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../../lib/mongodb";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    async session({ session, user }) {
+      session.user._id = user.id;
+      return session;
+    },
+  },
+};
+export default NextAuth(authOptions);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,38 @@
 import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+import BodyComposition from "@/components/BodyComposition/BodyComposition";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <div>
+        <BodyComposition />
+      </div>
+    );
+  }
+  return (
+    <>
+      <div
+        className="d-flex flex-column card mx-auto mt-5 p-2"
+        style={{ width: "30%" }}
+      >
+        <h1 className="text-center">Fitness App</h1>
+
+        <h4 className="text-center"> Not signed in </h4>
+        <p className="text-center">
+          to have access to your account, first sign in!
+        </p>
+
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={() => signIn()}
+        >
+          Sign in
+        </button>
+      </div>
+    </>
+  );
 }
