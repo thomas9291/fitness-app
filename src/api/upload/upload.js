7190704dddc966db050daa258482481503533fdd
thomas <130037065+thomas9291@users.noreply.github.com>
2023-07-11 16:16:03 +0200

    cloudinary run

---
+++ src/api/upload/upload.js
@@ -0,0 +1,52 @@
+import multer from "multer";
+import { handleUpload } from "../../../../helper";
+
+import Exercice from "../../../db/models/exercice";
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+const myUploadMiddleware = upload.single("sample_file");
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+//req.body.id
+const handler = async (req, res) => {
+  try {
+    await runMiddleware(req, res, myUploadMiddleware);
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    const cldRes = await handleUpload(dataURI);
+    res.json(cldRes);
+
+    if (cldRes) {
+      const exerciceToUpdate = await Exercice.updateOne(
+        { _id: req.query.id },
+        {
+          $addToSet: { images: cldRes.url },
+        }
+      );
+
+      return res.status(200).json(placeToUpdate);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({
+      message: error.message,
+    });
+  }
+};
+export default handler;
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
