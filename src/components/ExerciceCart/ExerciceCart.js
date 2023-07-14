a6024286f0fc986477b086ea7ec5118e0481889d
thomas <130037065+thomas9291@users.noreply.github.com>
2023-07-14 17:20:47 +0200

    fixing the get request

--- src/components/ExerciceCart/ExerciceCart.js
+++ src/components/ExerciceCart/ExerciceCart.js
@@ -4,7 +4,15 @@ import BodyComposition from "../BodyComposition/BodyComposition";
 import BodyCompositionPosterior from "../BodyComposition/BodyCompositionPosterior";
 /* import AddForm from "../AddForm/AddForm"; */
 
-export default function ExerciceCart({ onSubmit }) {
+export default function ExerciceCart({
+  onSubmit,
+  name,
+  type,
+  muscle,
+  difficulty,
+  equipment,
+  instructions,
+}) {
   const [hideBody, setHideBody] = useState(false);
   /* const [hideInputs, setHideInputs] = useState(false); */
   const handlerBodyComposition = () => {
@@ -17,14 +25,16 @@ export default function ExerciceCart({ onSubmit }) {
   return (
     <div className={classes.containerCart}>
       <h4>name</h4>
-      <img
+      {/* <img
         className={classes.image}
         src="https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZpdG5lc3N8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
         alt=""
-      />
+      /> */}
       <p>type</p>
       <p>muscle</p>
+      <p>equipment</p>
       <p>difficulty</p>
+      <p>instructions</p>
       {/*  <button className="btn btn-info m-2" onClick={handlerInputs}>
         {hideInputs ? "hide" : "result"}
       </button> */}
