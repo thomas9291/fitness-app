13849dd658215edf9ed36f0a38ed2447b658c0c5
thomas <130037065+thomas9291@users.noreply.github.com>
2023-09-04 11:34:22 +0200

    meta change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,11 @@ export default function App({
     <>
       <Head>
         <title>Fitness</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          http-equiv="Content-Security-Policy"
+          content="width=device-width, initial-scale=1, upgrade-insecure-requests"
+        />
 
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
